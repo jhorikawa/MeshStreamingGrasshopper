bd9ccb8ba2f3beb8e2cb8ba0e4d0a80326452552
jhorikawa <flowfreestar@gmail.com>
2017-03-04 07:14:53 +0900

    Added SendMeshComponent

---
+++ MeshStreaming/SendMeshComponent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace MeshStreaming
+{
+    public class SendMeshComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the SendMeshComponent class.
+        /// </summary>
+        public SendMeshComponent()
+          : base("SendMesh", "SendMesh",
+              "Send mesh through socket",
+              "MeshStreaming", "Data")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{690860ff-e5b0-4fcf-aff7-8c74a4be0579}"); }
+        }
+    }
+}
